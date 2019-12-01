8ea9317d007c906c35581379f2b424dafe1c9524
Agovpol <58398816+Agovpol@users.noreply.github.com>
2019-12-01 19:03:28 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "upobebe",
+    'summary': """Administracion UpoBebe""",
+    'description': """Administracion UpoBebe:clases, usuarios, material...""",
+    'author': "Grupo 9",
+    'category': 'UpoBebe',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': ['views/venta_view.xml','views/cliente_view.xml','views/articulo_view.xml'],
+    'demo': ['demo/upobebe_demo.xml'],
+    'application': True,
+}
